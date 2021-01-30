9b0694516775c32fc1201268f4f764419b0011d8
ctlaltdefeat <ctlaltdefeat@tfwno.gf>
2021-01-30 17:12:00 +0000

    added capability for balanced class training

--- train.py
+++ train.py
@@ -1,4 +1,12 @@
+from operator import itemgetter
+from pathlib import Path
+import pickle
+from typing import Iterator, List, Optional, Union
 import warnings
+import numpy as np
+from torch.utils.data.dataset import Dataset
+
+from torch.utils.data.sampler import Sampler
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
 import itertools
@@ -38,6 +46,152 @@ from utils import (
 torch.backends.cudnn.benchmark = True
 
 from adabelief_pytorch import AdaBelief
+
+
+class DatasetFromSampler(Dataset):
+    """Dataset to create indexes from `Sampler`.
+    Args:
+        sampler: PyTorch sampler
+    """
+
+    def __init__(self, sampler: Sampler):
+        """Initialisation for DatasetFromSampler."""
+        self.sampler = sampler
+        self.sampler_list = None
+
+    def __getitem__(self, index: int):
+        """Gets element of the dataset.
+        Args:
+            index: index of the element in the dataset
+        Returns:
+            Single element by index
+        """
+        if self.sampler_list is None:
+            self.sampler_list = list(self.sampler)
+        return self.sampler_list[index]
+
+    def __len__(self) -> int:
+        """
+        Returns:
+            int: length of the dataset
+        """
+        return len(self.sampler)
+
+
+class DistributedSamplerWrapper(DistributedSampler):
+    """
+    Wrapper over `Sampler` for distributed training.
+    Allows you to use any sampler in distributed mode.
+    It is especially useful in conjunction with
+    `torch.nn.parallel.DistributedDataParallel`. In such case, each
+    process can pass a DistributedSamplerWrapper instance as a DataLoader
+    sampler, and load a subset of subsampled data of the original dataset
+    that is exclusive to it.
+    .. note::
+        Sampler is assumed to be of constant size.
+    """
+
+    def __init__(
+        self,
+        sampler,
+        num_replicas: Optional[int] = None,
+        rank: Optional[int] = None,
+        shuffle: bool = True,
+    ):
+        """
+        Args:
+            sampler: Sampler used for subsampling
+            num_replicas (int, optional): Number of processes participating in
+              distributed training
+            rank (int, optional): Rank of the current process
+              within ``num_replicas``
+            shuffle (bool, optional): If true (default),
+              sampler will shuffle the indices
+        """
+        super(DistributedSamplerWrapper, self).__init__(
+            DatasetFromSampler(sampler),
+            num_replicas=num_replicas,
+            rank=rank,
+            shuffle=shuffle,
+        )
+        self.sampler = sampler
+
+    def __iter__(self):
+        """@TODO: Docs. Contribution is welcome."""
+        self.dataset = DatasetFromSampler(self.sampler)
+        indexes_of_indexes = super().__iter__()
+        subsampler_indexes = self.dataset
+        return iter(itemgetter(*indexes_of_indexes)(subsampler_indexes))
+
+
+class BalanceClassSampler(Sampler):
+    """Allows you to create stratified sample on unbalanced classes.
+    Args:
+        labels: list of class label for each elem in the dataset
+        mode: Strategy to balance classes.
+            Must be one of [downsampling, upsampling]
+    """
+
+    def __init__(
+        self, labels: List[int], mode: Union[str, int] = "downsampling"
+    ):
+        """Sampler initialisation."""
+        super().__init__(labels)
+
+        labels = np.array(labels)
+        samples_per_class = {
+            label: (labels == label).sum() for label in set(labels)
+        }
+
+        self.lbl2idx = {
+            label: np.arange(len(labels))[labels == label].tolist()
+            for label in set(labels)
+        }
+
+        if isinstance(mode, str):
+            assert mode in ["downsampling", "upsampling"]
+
+        if isinstance(mode, int) or mode == "upsampling":
+            samples_per_class = (
+                mode
+                if isinstance(mode, int)
+                else max(samples_per_class.values())
+            )
+        else:
+            samples_per_class = min(samples_per_class.values())
+
+        self.labels = labels
+        self.samples_per_class = samples_per_class
+        self.length = self.samples_per_class * len(set(labels))
+
+    def __iter__(self) -> Iterator[int]:
+        """
+        Yields:
+            indices of stratified sample
+        """
+        indices = []
+        for key in sorted(self.lbl2idx):
+            replace_flag = self.samples_per_class > len(self.lbl2idx[key])
+            indices += np.random.choice(
+                self.lbl2idx[key], self.samples_per_class, replace=replace_flag
+            ).tolist()
+        assert len(indices) == self.length
+        np.random.shuffle(indices)
+
+        return iter(indices)
+
+    def __len__(self) -> int:
+        """
+        Returns:
+             length of result sample
+        """
+        return self.length
+
+    def _get_label(self, dataset, idx):
+        if self.callback_get_label:
+            return self.callback_get_label(dataset, idx)
+        else:
+            raise NotImplementedError
 
 
 def train(rank, a, h):
@@ -136,13 +290,22 @@ def train(rank, a, h):
         h.fmin,
         h.fmax,
         n_cache_reuse=0,
-        shuffle=False if h.num_gpus > 1 else True,
+        shuffle=False,
         fmax_loss=h.fmax_for_loss,
         device=device,
         fine_tuning=a.fine_tuning,
         base_mels_path=a.input_mels_dir,
     )
 
+    # train_sampler = DistributedSamplerWrapper(
+    #     BalanceClassSampler(
+    #         [
+    #             s[-1]
+    #             for s in pickle.load(open(Path(a.input_training_file), "rb"))
+    #         ],
+    #         "downsampling",
+    #     ),
+    # )
     train_sampler = DistributedSampler(trainset) if h.num_gpus > 1 else None
 
     train_loader = DataLoader(
@@ -174,11 +337,26 @@ def train(rank, a, h):
             fine_tuning=a.fine_tuning,
             base_mels_path=a.input_mels_dir,
         )
+        validation_sampler = BalanceClassSampler(
+            [
+                s[-1]
+                for s in pickle.load(open(Path(a.input_validation_file), "rb"))
+            ],
+            "downsampling",
+        )
+        # validation_loader = DataLoader(
+        #     validset,
+        #     num_workers=1,
+        #     shuffle=False,
+        #     sampler=validation_sampler,
+        #     batch_size=1,
+        #     pin_memory=True,
+        #     drop_last=True,
+        # )
         validation_loader = DataLoader(
             validset,
             num_workers=1,
             shuffle=False,
-            sampler=None,
             batch_size=1,
             pin_memory=True,
             drop_last=True,
